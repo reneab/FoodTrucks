6a5c729e1203b9fa3fdddf1cc8bcb754f7153c22
reneab <rene.abd@gmail.com>
2021-01-17 15:34:53 +0200

    put in aws folder

--- aws-ecs/setup-aws-ecs.sh
+++ aws-ecs/setup-aws-ecs.sh
@@ -7,7 +7,7 @@ ecs-cli configure --region us-east-1 --cluster foodtrucks
 ecs-cli up --keypair ecs --capability-iam --size 1 --instance-type t2.medium
 
 # deploy
-cd aws-ecs && ecs-cli compose --file aws-compose.yml up
+ecs-cli compose --file docker-compose.yml up
 
 # check
 ecs-cli ps
